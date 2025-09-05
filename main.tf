378253a56acc9f117bca6c171dfdc6e306341699
EC2 Default User <ec2-user@ip-172-31-45-157.us-east-2.compute.internal>
2025-09-05 18:05:48 +0000

    modified

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable "secret_value" {
 
 resource "null_resource" "ci10_demo" {
   provisioner "local-exec" {
-    command = "echo 'Simulated deployment with secret: ${var.secret_value}' >> $GITHUB_WORKSPACE/deployment.log"
+    command = "echo 'Simulated deployment with secret: ${var.secret_value}' >> ./deployment.log"
   }
+}
 }
