d7cecce3889fc15b2a89552a1fb00045e8ccff65
EC2 Default User <ec2-user@ip-172-31-45-157.us-east-2.compute.internal>
2025-09-05 18:07:52 +0000

    done

--- main.tf
+++ main.tf
@@ -1,14 +1,19 @@
+# main.tf
+
+# Provider (null provider used to simulate deployment)
 provider "null" {}
 
+# Sensitive variable
 variable "secret_value" {
   description = "A sensitive value from CI/CD"
   default     = "super-secret-value"
   sensitive   = true
 }
 
+# Null resource to simulate deployment
 resource "null_resource" "ci10_demo" {
   provisioner "local-exec" {
+    # Write the secret to deployment.log in current working directory
     command = "echo 'Simulated deployment with secret: ${var.secret_value}' >> ./deployment.log"
   }
-}
 }
