7d48c7a87c9f4105054596b10efbffab984283c1
EC2 Default User <ec2-user@ip-172-31-45-157.us-east-2.compute.internal>
2025-09-05 18:16:46 +0000

    hello done

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ variable "secret_value" {
   sensitive   = true
 }
 
-# Use a local file resource instead of local-exec
+# Create deployment.log reliably
 resource "local_file" "deployment_log" {
   content  = "Simulated deployment with secret: ${var.secret_value}"
   filename = "deployment.log"
